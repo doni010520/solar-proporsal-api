7f48114c1395c06e98474e302d198f2654ba834c
doni010520 <donikasumii@gmail.com>
2025-10-17 16:15:09 -0300

    Adjust logo position in PDF generator

--- app/pdf/generator.py
+++ app/pdf/generator.py
@@ -612,7 +612,7 @@ class PDFGenerator:
             logos_path = os.path.join(self.assets_path, "logos_fornecedores.png")
             if os.path.exists(logos_path):
                 # Desenhar maior e centralizado
-                c.drawImage(logos_path, 320, y_pos, width=width-740, height=400, 
+                c.drawImage(logos_path, 120, y_pos, width=width-740, height=400, 
                            preserveAspectRatio=True, mask='auto')
         except:
             # Se não houver imagem, listar os nomes
