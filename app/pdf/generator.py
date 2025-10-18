a8526f2dc65104f59a2a9a341041c5f64807aaa1
doni010520 <donikasumii@gmail.com>
2025-10-18 09:35:16 -0300

    Update generator.py

--- app/pdf/generator.py
+++ app/pdf/generator.py
@@ -284,7 +284,7 @@ class PDFGenerator:
         
         y_pos = height - 160
         y_pos = self._draw_section_header(c, y_pos, "Dados da Proposta", width)
-        y_pos -= self.SPACE_MEDIUM
+        y_pos -= self.SPACE_LARGE  # Aumentado de SPACE_MEDIUM para SPACE_LARGE
 
         c.setFont(self.FONT_NORMAL, self.FONT_SIZE_BODY)
         c.setFillColor(self.COLOR_TEXT)
@@ -296,9 +296,9 @@ class PDFGenerator:
             c.drawString(220, y_pos, str(valor))
             y_pos -= self.LINE_SPACING
         
-        y_pos -= self.SPACE_SMALL
+        y_pos -= self.SPACE_MEDIUM  # Aumentado de SPACE_SMALL para SPACE_MEDIUM
         y_pos = self._draw_section_header(c, y_pos, "Perfil de Consumo do Cliente", width)
-        y_pos -= self.SPACE_MEDIUM
+        y_pos -= self.SPACE_LARGE  # Aumentado de SPACE_MEDIUM para SPACE_LARGE
         
         dados_consumo = [
             ("CONCESSIONÁRIA", "CPFL"), ("TIPO DE FORNECIMENTO", dados_sistema.get('tipo_fornecimento', 'N/A')),
@@ -312,9 +312,9 @@ class PDFGenerator:
             c.drawString(320, y_pos, str(valor))
             y_pos -= self.LINE_SPACING
             
-        y_pos -= self.SPACE_SMALL
+        y_pos -= self.SPACE_MEDIUM  # Aumentado de SPACE_SMALL para SPACE_MEDIUM
         y_pos = self._draw_section_header(c, y_pos, "Sistema Fotovoltaico Proposto", width)
-        y_pos -= self.SPACE_MEDIUM
+        y_pos -= self.SPACE_LARGE  # Aumentado de SPACE_MEDIUM para SPACE_LARGE
 
         dados_sfv = [
             ("NÚMERO DE MÓDULOS (un.)", f"{int(dados_sistema.get('num_modulos', 0))}"),
